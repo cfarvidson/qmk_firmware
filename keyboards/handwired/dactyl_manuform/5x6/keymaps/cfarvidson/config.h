a1b7ec9774182e48eabff774e9d7989892faf15b
Carl-Fredrik Arvidson <carl-fredrik@arvidson.io>
2021-05-17 20:50:23 +0200

    Create combos for question mark and exclamation

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/cfarvidson/config.h
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/cfarvidson/config.h
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 #define USE_SERIAL
-#define COMBO_COUNT 5
+#define COMBO_COUNT 7
 
 #define MASTER_LEFT
 // #define MASTER_RIGHT
--- keyboards/handwired/dactyl_manuform/5x6/keymaps/cfarvidson/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/cfarvidson/keymap.c
@@ -1,19 +1,19 @@
 /* A standard layout for the Dactyl Manuform 5x6 Keyboard */ 
 
 #include QMK_KEYBOARD_H
-
+#include "keymap_swedish.h"
 
 #define _QWERTY 0
 #define _MOUSE 1
 #define _ARROWS 2
 #define _CODING 3
-#define _NUMBERS 4
+#define _TAB 4
 #define _WORDNAV 5
 #define _POWERNAV 6
 #define _APPS 7
 
 #define CODING MO(_CODING)
-#define NUMBERS MO(_NUMBERS)
+#define TAB MO(_NUMBERS)
 #define POWERNAV MO(_POWERNAV)
 #define RIGHT_THUMB LT(_WORDNAV,KC_BSPC)
 
@@ -23,6 +23,8 @@ enum combos {
   CV_PASTE,
   XV_PASTE_MATCH,
   ZV_SELECT_ALL,
+  MCOMM_QUES,
+  COMMDOT_EXLM,
 };
 
 // Hands down combos https://sites.google.com/alanreiser.com/handsdown/home#h.aplo097wq6hh
@@ -31,6 +33,8 @@ const uint16_t PROGMEM copy_combo[] = {KC_X, KC_C, COMBO_END};
 const uint16_t PROGMEM paste_combo[] = {KC_C, KC_V, COMBO_END};
 const uint16_t PROGMEM paste_match_combo[] = {KC_X, KC_V, COMBO_END};
 const uint16_t PROGMEM select_all_combo[] = {KC_Z, KC_V, COMBO_END};
+const uint16_t PROGMEM question_mark_combo[] = {KC_M, KC_COMM, COMBO_END};
+const uint16_t PROGMEM exclamation_mark_combo[] = {KC_COMM, KC_DOT, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
    [ZC_CUT] = COMBO(cut_combo, LGUI(KC_X)),
@@ -38,6 +42,8 @@ combo_t key_combos[COMBO_COUNT] = {
    [CV_PASTE] = COMBO(paste_combo, LGUI(KC_V)),
    [XV_PASTE_MATCH] = COMBO(paste_match_combo, LGUI(LSA(KC_V))),
    [ZV_SELECT_ALL] = COMBO(select_all_combo, LGUI(KC_A)),
+   [MCOMM_QUES] = COMBO(question_mark_combo, SE_QUES),
+   [COMMDOT_EXLM] = COMBO(exclamation_mark_combo, SE_EXLM)
 };
 
 
@@ -45,8 +51,8 @@ combo_t key_combos[COMBO_COUNT] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_5x6(
-     KC_ESC     , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8    , KC_9   , KC_0     , KC_MINS,
-     NUMBERS    , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I    , KC_O   , KC_P     , KC_LBRC,
+     KC_ESC     , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8    , KC_9   , KC_0     , SE_APOS,
+     TAB    , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I    , KC_O   , KC_P     , KC_LBRC,
      CODING     , KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K    , KC_L   , KC_SCLN  , KC_QUOT,
      KC_LSFT    , KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  , KC_COMM , KC_DOT , KC_SLSH  , KC_LSFT,
                       _______, MO(_ARROWS),                                                       KC_NO   , LSFT(KC_NUHS),
@@ -92,11 +98,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                  
   ),
 
-    [_NUMBERS] = LAYOUT_5x6(
+    [_TAB] = LAYOUT_5x6(
      RESET  ,_______,_______,_______,_______,_______,                         _______,_______,_______,_______,_______,_______,
-     _______,MEH(KC_Q),MEH(KC_W),MEH(KC_E),MEH(KC_R),MEH(KC_T),                         LSFT(KC_MINS), KC_P7  , KC_P8 , KC_P9 ,_______,_______,
+     _______,MEH(KC_Q),MEH(KC_W),MEH(KC_E),MEH(KC_R),MEH(KC_T),               KC_NO  , KC_P7  , KC_P8 , KC_P9 ,_______,_______,
      _______,MEH(KC_A),MEH(KC_S),MEH(KC_D),MEH(KC_F),MEH(KC_G),               _______, KC_P4  , KC_P5 , KC_P6 ,_______,_______,
-     _______,_______,_______,_______,_______,_______,                         LSFT(KC_1), KC_P1  , KC_P2 , KC_P3 ,_______,_______,
+     _______,_______,_______,_______,_______,_______,                         KC_NO  , KC_P1  , KC_P2 , KC_P3 ,_______,_______,
                        _______,_______,                                                           _______,_______,
                                           LGUI(KC_NUBS),_______,                  _______,_______,
                                           _______,_______,                        _______,_______,
