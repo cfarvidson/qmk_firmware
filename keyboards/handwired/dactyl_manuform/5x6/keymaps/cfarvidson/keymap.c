2290dec3875065090330ad6a487b746547bac4e6
Carl-Fredrik Arvidson <carl-fredrik@arvidson.io>
2021-05-18 15:26:28 +0200

    Remove the custom shift stuff

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/cfarvidson/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/cfarvidson/keymap.c
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_ESC    , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         TD(TD_6_CAPS) , KC_7  , KC_8    , KC_9   , KC_0     , SE_APOS,
      TAB       , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I    , KC_O   , KC_P     , KC_LBRC,
      CODING    , KC_A  , KC_S  , KC_D  , KC_F, KC_G  ,                           KC_H, KC_J, KC_K    , KC_L   , KC_SCLN  , KC_QUOT,
-     KC_LSFT   , KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  , KC_COMM , KC_DOT , LSFT_T(KC_SLSH)  ,DO_NOTHING,
+     KC_LSFT   , KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  , KC_COMM , KC_DOT , KC_SLSH  ,KC_LSFT,
                         _______, MO(_ARROWS),                                                    KC_NO   , KC_NO,
                                       LCTL_T(KC_TAB),RIGHT_THUMB,                 KC_ENT, LT(_MOUSE,KC_SPC),
                                       LM(_APPS,MOD_LGUI),KC_LALT,                 KC_PAST, MEH(KC_NO),
