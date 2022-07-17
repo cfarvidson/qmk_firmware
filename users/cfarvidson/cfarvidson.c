ee2c8a7b8b868aeeebd36bb5dc5be3fd817f2fe1
Carl-Fredrik Arvidson <carl-fredrik@arvidson.io>
2022-07-17 19:27:05 +0200

    wip: try out the mouse 1 on the home thumb

--- users/cfarvidson/cfarvidson.c
+++ users/cfarvidson/cfarvidson.c
@@ -173,7 +173,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        XXXXXXX, _______, DRGSCRL, SNIPING, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, SNIPING, DRGSCRL, _______, XXXXXXX,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                  KC_BTN1, KC_BTN2, KC_BTN3,    KC_BTN3, KC_BTN1, XXXXXXX
+                                  KC_BTN2, KC_BTN1, KC_BTN3,    KC_BTN3, KC_BTN1, XXXXXXX
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
