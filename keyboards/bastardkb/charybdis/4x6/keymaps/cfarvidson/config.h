b77e31bacc8a9e193645ff39e23fca2579156317
Carl-Fredrik Arvidson <carl-fredrik@arvidson.io>
2023-05-22 13:37:58 +0200

    restore tapping term to default value

--- keyboards/bastardkb/charybdis/4x6/keymaps/cfarvidson/config.h
+++ keyboards/bastardkb/charybdis/4x6/keymaps/cfarvidson/config.h
@@ -40,7 +40,7 @@
 
 // default but used in macros
 #undef TAPPING_TERM
-#define TAPPING_TERM 175
+#define TAPPING_TERM 200
 #define TAPPING_TERM_PER_KEY
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
