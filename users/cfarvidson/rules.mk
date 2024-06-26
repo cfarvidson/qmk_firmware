b4eea06b459e082af1b99b794cacb1501bbf6196
Carl-Fredrik Arvidson <carl-fredrik@arvidson.io>
2024-06-26 15:41:13 +0200

    Disable autoshift

--- users/cfarvidson/rules.mk
+++ users/cfarvidson/rules.mk
@@ -9,7 +9,7 @@ COMMAND_ENABLE = no         # Commands for debug and configuration
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 NKRO_ENABLE = yes            # USB Nkey Rollover
-AUTO_SHIFT_ENABLE = yes # Auto Shift
+AUTO_SHIFT_ENABLE = no # Auto Shift
 COMBO_ENABLE = yes
 
 # Disable lighting since you don't ever use on any keyboard
